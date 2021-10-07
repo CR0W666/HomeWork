46716e7290d6808ec1d3706fb498cd5d1e96bfd5
Lukáš Petráček <44005888+CR0W666@users.noreply.github.com>
2021-10-07 14:40:35 +0200

    Add files via upload

---
+++ C4/PRG/test2021_10_7/test/src/test2021_10_7.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class test2021_10_7 {
+    public static void main(String[] args) {
+        int input = input("enter a number \n");
+        System.out.println("input is: "+input);
+        findArmstrongsUpTo(input);
+    }
+
+    public static int input(String msg) {
+        System.out.println(msg);
+        try (Scanner sc = new Scanner(System.in)) {
+            return sc.nextInt();
+        } catch (InputMismatchException e) {
+            return 1000;
+        }
+    }
+
+    public static void findArmstrongsUpTo(int ceil) {
+        for (int i = 1; i < ceil; i++) {
+            if(isArmstrong(i)) System.out.println(i);
+        }
+    }
+
+    public static boolean isArmstrong(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        int sum = 0;
+        for (char digit : digits) {
+            int idigit = Character.getNumericValue(digit);
+            sum += Math.pow(idigit, 3);
+        }
+        return sum == num;
+    }
+}
