2eb7312fc68d8f21ad51c9666dd7a4ecbb2e5e92
Lukáš Petráček <44005888+CR0W666@users.noreply.github.com>
2021-10-21 14:44:45 +0200

    Update test21_10_2021.java

--- C4/PRG/test21_10_2021/test21_10_2021.java
+++ C4/PRG/test21_10_2021/test21_10_2021.java
@@ -4,18 +4,15 @@ public class test21_10_2021 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter how many numbers you want");
         int numCount = sc.nextInt();
+        double avg = 0;
         int[] nums = new int[numCount];
         for (int i = 0; i < numCount; i++) {
             System.out.println("Enter number " + (i+1) +".");
             nums[i] = sc.nextInt();
+            avg += nums[i];
         }
         sc.close();
-        double avg = 0;
-        for (int num : nums) {
-            avg += num;
-        }
         avg /= Math.ceil(nums.length);
-
         int sum = 0;
         for (int num : nums) {
             if(num >= avg) sum += num;
