c883009c595a6e2a0e55bffad495361b046418d0
CR0W666 <lukas.petracek@educanet.cz>
2021-11-10 12:05:26 +0100

    Primes finished

---
+++ C4/PRG/primes/Primes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Primes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadejte cislo do ktereho chcete najit prvocisla");
+        findPrimes(sc.nextInt());
+    }
+
+    public static boolean isPrime(int number) {
+        if(number==1) return false;
+        for(int i=2; i < number; i++) {
+            if(number%i==0) return false;
+        }
+        return true;
+    }
+
+    public static void findPrimes(int ceil) {
+        System.out.println("--------------------\nPrimes up to " + ceil + ": ");
+        for(int i=2; i <= ceil; i++) {
+            if(isPrime(i)) System.out.println(i);
+        }
+    }
+
+
+}
