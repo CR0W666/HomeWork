c642402a383fc4f0566431ec35d5b0dc8794680d
CR0W666 <lukas.petracek@educanet.cz>
2021-11-19 13:43:37 +0100

    the craziest and dirtiest code i have ever written

---
+++ C4/PRG/logistic_company/Car.java
@@ -0,0 +1,29 @@
+package logistic_company;
+
+public class Car {
+    private int id;
+    private double carryWeight;
+    private double weight;
+
+    public Car(int id, double carryWeight, double weight) {
+        this.id = id;
+        this.carryWeight = carryWeight;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double getCarryWeight() {
+        return this.carryWeight;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
