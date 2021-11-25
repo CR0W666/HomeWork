2740e9c030aa202235430df632c3e1e5c91ad25f
CR0W <44005888+CR0W666@users.noreply.github.com>
2021-11-25 20:26:53 +0100

    keyword final, stylistic changes

--- C4/PRG/zalobci/Zalobci.java
+++ C4/PRG/zalobci/Zalobci.java
@@ -9,34 +9,39 @@ import java.util.Map;
 import java.util.Scanner;
 
 /*
-* Zpracujte soubor naklady_zastoupeni.csv (nebo můžete mít nahardcoděno v aplikaci).
-* V něm jsou 3 údaje - rok, jméno a částka.
-* Každý řádek vhodně reprezentujte třídou a uložte do kolekce..
-! 1. Spočítejte průměrnou částku sporu (zaokrouhlenou na celé 100 koruny, inflaci ignorujme).
-! 2. Spočítejte, který žalobce "stál" celkově nejvíc. (Tj. součet částek všech jeho sporů), a kolik byla jeho celková "cena"
-*/
+ * Zpracujte soubor naklady_zastoupeni.csv (nebo můžete mít nahardcoděno v aplikaci). V něm jsou 3
+ * údaje - rok, jméno a částka. Každý řádek vhodně reprezentujte třídou a uložte do kolekce.. ! 1.
+ * Spočítejte průměrnou částku sporu (zaokrouhlenou na celé 100 koruny, inflaci ignorujme). ! 2.
+ * Spočítejte, který žalobce "stál" celkově nejvíc. (Tj. součet částek všech jeho sporů), a kolik
+ * byla jeho celková "cena"
+ */
 
 public class Zalobci {
     public static void main(String[] args) throws FileNotFoundException {
         final List<Disputee> disputes = initDisputes();
+        
         final double totalAvg = avgDisputeCosts(disputes);
+        System.out.println("Prumerna castka sporu: " + Math.floor(totalAvg / 100) + " Stovek ("+ totalAvg + "kc)");
+
         final Disputee mostExpensiveDisputee = mostExpensiveDisputee(groupByName(disputes));
-        System.out.println(totalAvg + " | " + Math.floor(totalAvg/100) + " Stovek");
-        System.out.println("Nejdrazsi spor je od " + mostExpensiveDisputee.name + ": " + mostExpensiveDisputee.cost + "kc");
+        final String name = mostExpensiveDisputee.name;
+        final Long cost = mostExpensiveDisputee.cost;
+        System.out.println("Nejdrazsi spor je od " + name + ": " + cost + "kc");
     }
 
 
     public static List<Disputee> initDisputes() throws FileNotFoundException {
-        List<Disputee> disputes = new ArrayList<>();
-        
-        Scanner sc = new Scanner(new File("C4\\PRG\\zalobci\\naklady_zastoupeni.csv"));
-        sc.nextLine(); //Skip header
+        final List<Disputee> disputes = new ArrayList<>();
+
+        final Scanner sc = new Scanner(new File("C4\\PRG\\zalobci\\naklady_zastoupeni.csv"));
+        sc.nextLine(); // Skip header
+
         while (sc.hasNext()) {
-            String[] line = sc.nextLine().split(",");
+            final String[] line = sc.nextLine().split(",");
             line[2] = line[2].replace(".00", "");
-            
+
             // !hrozne dirty
-            if(line[2].contains("Finspel")) {
+            if (line[2].contains("Finspel")) {
                 line[1] += "," + line[2];
                 line[2] = line[3].replace(".00", "");
             }
@@ -51,13 +56,13 @@ public class Zalobci {
     }
 
     public static Map<String, Long> groupByName(List<Disputee> disputes) {
-        
-        Map<String, Long> grouped = new HashMap<>();
-        
+
+        final Map<String, Long> grouped = new HashMap<>();
+
         for (Disputee dispute : disputes) {
-            if(grouped.containsKey(dispute.name)) 
+            if (grouped.containsKey(dispute.name))
                 grouped.put(dispute.name, grouped.get(dispute.name) + dispute.cost);
-            else 
+            else
                 grouped.put(dispute.name, dispute.cost);
         }
 
@@ -66,21 +71,21 @@ public class Zalobci {
     }
 
     public static Disputee mostExpensiveDisputee(Map<String, Long> disputees) {
-        Map.Entry<String, Long> mostExpensiveDisputee = disputees.entrySet()
-                                                                 .stream()
-                                                                 .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get();
-        String name = mostExpensiveDisputee.getKey();
-        Long cost = mostExpensiveDisputee.getValue();
+        final Map.Entry<String, Long> mostExpensiveDisputee = disputees.entrySet().stream()
+                .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get();
+        final String name = mostExpensiveDisputee.getKey();
+        final Long cost = mostExpensiveDisputee.getValue();
 
         return new Disputee("null", name, cost);
     }
+
     public static double avgDisputeCosts(List<Disputee> disputes) {
         long sum = 0;
         for (Disputee disputee : disputes) {
             sum += disputee.cost;
         }
-        
+
         return ((double) sum / disputes.size());
-    } 
+    }
 
 }
